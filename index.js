35ff74615a7cccfb2e38be3c4d498609b5ff8bdb
cassiano belniak <cassi.belniak@gmail.com>
2020-10-01 18:52:58 -0300

    Added values calculation

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-// Const declarations
+// Constants declarations
 const xlsx = require('xlsx');
-const FILE_PATH = "test-fil.xlsx"
+const FILE_PATH = "test-file.xlsx"
 
+const INSCRITION_COLUMN = "A";
+const NAME_COLUMN = "B";
+const ABSENCES_COLUMN = "C";
+const P1_GRADE_COLUMN = "D";
+const P2_GRADE_COLUMN = "E";
+const P3_GRADE_COLUMN = "F";
+const STARTING_INDEX = 4;
+const ENDING_INDEX = 28;
+
+const TOTAL_CLASSES = 60;
+
+const SITUATION = {'APPROVED': 0, 'DISAPPROVED_BY_GRADE':1, 'DISAPPROVED_BY_FREQUENCY':2, 'FINAL': 3};
+
+var p = console.log;
 
 // Program main execution
-var workbook = readFile();
-var sheet = getFirstSheet(workbook);
+try {
+    console.log('Starting program');
+    console.log(`Reading ${FILE_PATH} file`);
+    var workbook = readFile();
+    var sheet = getFirstSheet(workbook);
+    console.log('Calculation results...');
+    var results = getResults(sheet);
+    showResults(results);
+} catch (e) {
+    console.error(e);
+}
 
 
 // Function Declaration
@@ -13,12 +36,60 @@ function readFile(){
     try {
         return xlsx.readFile(FILE_PATH);
     } catch (e) {
-        throw `Error reading ${FILE_PATH} file` 
+        throw `Error reading ${FILE_PATH} file`; 
     }
 }
 
 function getFirstSheet(workbook){
     if (workbook.Sheets.length === 0)
-        throw "The file does not contains any sheet"
+        throw "The file does not contains any sheet";
     return workbook.Sheets[workbook.SheetNames[0]];
+}
+
+function getCellValue(sheet, cell){
+    if (!sheet[cell])
+        throw `The cell ${cell} does not contains any value`;
+    return sheet[cell].v;
+}
+
+function getResults(sheet){
+    var results = [];
+    for (row = STARTING_INDEX; row < ENDING_INDEX; row++){
+        var name = getCellValue(sheet, NAME_COLUMN + row);
+        var inscritionNumber = getCellValue(sheet, INSCRITION_COLUMN + row);
+        var p1 = getCellValue(sheet, P1_GRADE_COLUMN + row);
+        var p2 = getCellValue(sheet, P2_GRADE_COLUMN + row);
+        var p3 = getCellValue(sheet, P3_GRADE_COLUMN + row);
+        var absences = getCellValue(sheet, ABSENCES_COLUMN + row);
+        var averageGrade = (p1 + p2 + p3)/3;
+        console.log(`${p1} ${p2} ${p3} ${averageGrade}`);
+        var situation = getSituation(averageGrade, absences);
+        var finalGrade = getFinalGrade(situation, averageGrade);
+        results.push({row, name, inscritionNumber, averageGrade, situation, finalGrade, absences});
+    }
+    return results;
+}
+
+function getSituation(averageGrade, absences){
+    if (absences/TOTAL_CLASSES > 0.25)
+        return SITUATION.DISAPPROVED_BY_FREQUENCY;
+    if (averageGrade < 50)
+        return SITUATION.DISAPPROVED_BY_GRADE;
+    if (averageGrade < 70)
+        return SITUATION.FINAL;
+    return SITUATION.APPROVED;
+}
+
+function getFinalGrade(situation, averageGrade){
+    if (situation !== SITUATION.FINAL)
+        return 0;
+    return 100-averageGrade; //(5 <= (m + naf)/2)
+}
+
+function showResults(results){
+    console.log("Results:");
+    console.log(results);
+    results.forEach(result=>{
+        console.log(`${result.inscritionNumber} - ${result.name}: Nota para a aprovação final - ${result.finalGrade}`);
+    });
 }
